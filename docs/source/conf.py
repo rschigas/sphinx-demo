8d829582f91cc2c960b057ff1d120eaf2f9b0f68
Roland Schigas <rschigas@eos.ubc.ca>
2020-09-17 15:45:23 -0700

    Replace wfrt.yml without build info, recreate conda environment with all dependencies, add RTD theme & Sphinx extensions, rebuild

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-
 # -- Project information -----------------------------------------------------
 
 project = 'Schig Docs'
@@ -24,14 +23,21 @@ author = 'R. Schigas'
 # The full version, including alpha/beta/rc tags
 release = '0.1'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.autosectionlabel', \
+    'sphinx.ext.napoleon', 'sphinx_automodapi.automodapi', 'recommonmark', \
+        'sphinx_markdown_tables', 'sphinxcontrib.inlinesyntaxhighlight', \
+            'nbsphinx', 'sphinx.ext.mathjax']
+
+# use language set by highlight directive if no language is set by role
+inline_highlight_respect_highlight = False
+
+# use language set by highlight directive if no role is set
+inline_highlight_literals = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -39,17 +45,17 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+# # Add any paths that contain custom static files (such as style sheets) here,
+# # relative to this directory. They are copied after the builtin static files,
+# # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
